2993ade4b3bdaede0cc928b07e45ce23088e5930
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2019-12-17 22:08:05 +0900

    change undo/redo package

--- conf/11-undo-redo.el
+++ conf/11-undo-redo.el
@@ -1,8 +1,3 @@
-;; redo+
-(require 'redo+)
-(global-set-key (kbd "C-?") 'redo)
-
-;; 過去のundoをredoしない
-;(setq undo-no-redo t)
-(setq undo-limit 600000)
-(setq undo-strong-limit 900000)
+;; undo-tree
+(require 'undo-tree)
+(global-undo-tree-mode)
