080f1fd226d350ea45d4adf5ef20902a71e5626d
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-01-06 14:10:43 +0900

    refactor

--- conf/20-functions-typescript.el
+++ conf/20-functions-typescript.el
@@ -8,7 +8,6 @@
   ;; company is an optional dependency. You have to
   ;; install it separately via package-install
   ;; `M-x package-install [ret] company`
-  (company-mode +1))
-
-;; aligns annotation to the right hand side
-(setq company-tooltip-align-annotations t)
+  (company-mode +1)
+  ;; aligns annotation to the right hand side
+  (setq-local company-tooltip-align-annotations t))
