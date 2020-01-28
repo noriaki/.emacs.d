c6b6f3f598b0a3014be13c228bc80a7a6d58f65d
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-01-28 18:02:15 +0900

    add company-css backend to css|scss-mode

--- conf/21-scss.el
+++ conf/21-scss.el
@@ -3,7 +3,8 @@
 
 (defun noriaki/custom-scss-mode-hooks ()
   "scss-mode-hook"
-  (set (make-local-variable 'css-indent-offset) 2))
+  (set (make-local-variable 'css-indent-offset) 2)
+  (add-to-list (make-local-variable 'company-backends) 'company-css))
 (add-hook 'scss-mode-hook 'noriaki/custom-scss-mode-hooks)
 
 ;; 自動コンパイルをオフにする
--- transient/history.el
+++ transient/history.el
@@ -1,1 +1,6 @@
-nil
+((magit-commit nil)
+ (magit-diff
+  ("--no-ext-diff" "--stat")
+  ("--no-ext-diff"))
+ (magit-dispatch nil)
+ (magit-fetch nil))
